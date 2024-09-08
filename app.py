ece97657f0770de1fbb49a668f5f8070036954bf
Zachary Richman <88895551+Zachary-Richman@users.noreply.github.com>
2024-09-07 23:32:57 -0400

    removed @property

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ class time:
         pass
 
     # Function to get current time as (hh:mm AM/PM)
-    @property
     def current_time(self):
         return datetime.now().strftime("%I:%M %p")
 
     # Function to get current date as (Y:m:d)
-    @property
     def current_date(self):
         return datetime.now().strftime("%Y:%m:%d")
