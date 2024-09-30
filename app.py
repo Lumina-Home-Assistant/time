39404efa5094ddbd51a0ac51aa04b8f8ce08f8c9
Zachary Richman <88895551+Zachary-Richman@users.noreply.github.com>
2024-09-30 16:39:51 -0400

    @staticmethod

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ class time:
         pass
 
     # Function to get current time as (hh:mm AM/PM)
+    @staticmethod
     def current_time(self):
         return datetime.now().strftime("%I:%M %p")
 
     # Function to get current date as (Y:m:d)
+    @staticmethod
     def current_date(self):
         return datetime.now().strftime("%Y:%m:%d")
